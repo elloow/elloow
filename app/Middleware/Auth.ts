d5981ef94cf7ad05f3afa8eb8f36d4488b03d62f
CreB <bastien.crettenand@hotmail.com>
2020-09-23 11:40:13 +0200

    fix eslint

--- app/Middleware/Auth.ts
+++ app/Middleware/Auth.ts
@@ -23,16 +23,16 @@ export default class AuthMiddleware {
    * during the current request.
    */
   protected async authenticate (auth: HttpContextContract['auth'], guards: any[]) {
-  	/**
+    /**
   	 * Hold reference to the guard last attempted within the for loop. We pass
   	 * the reference of the guard to the "AuthenticationException", so that
   	 * it can decide the correct response behavior based upon the guard
   	 * driver
   	 */
-  	let guardLastAttempted: string | undefined
+    let guardLastAttempted: string | undefined
 
     for (let guard of guards) {
-    	guardLastAttempted = guard
+      guardLastAttempted = guard
 
       if (await auth.use(guard).check()) {
         /**
