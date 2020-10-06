eaf8739646086a167033d0fa29d3228f30b6781b
CreB <bastien.crettenand@hotmail.com>
2020-10-06 20:48:12 +0200

    Affiliation rights can be null

--- database/migrations/1602004579336_affiliations.ts
+++ database/migrations/1602004579336_affiliations.ts
@@ -6,7 +6,7 @@ export default class Affiliations extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('id')
-      table.text('rights', 'longtext').notNullable().defaultTo(JSON.stringify({}))
+      table.text('rights', 'longtext')
       table.integer('user_id').unsigned().references('id').inTable('users').onDelete('CASCADE')
       table.integer('organisation_id').unsigned().references('id').inTable('organisations').onDelete('CASCADE')
       table.unique(['user_id', 'organisation_id'])
