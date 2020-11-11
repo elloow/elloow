aa57905bf09c7252b7282b3a90d2a0cab6c4b24c
crbast <bastien.crettenand@hotmail.com>
2020-11-11 12:29:37 +0000

    Add organisation register basic mail template

--- app/Mailers/RegisterOrganisation.ts
+++ app/Mailers/RegisterOrganisation.ts
@@ -3,8 +3,7 @@ import Mail, { MessageContract } from '@ioc:Adonis/Addons/Mail'
 export default class RegisterOrganisation {
   private from = 'info@crbast.ch'
   private subject = 'Create your elloow organisation'
-  private template = 'emails/register-organisation'
-  // TODO create html template
+  private template = 'emails/register_organisation'
 
   private templateData = {
     actionToken: this.actionToken,
@@ -15,12 +14,12 @@ export default class RegisterOrganisation {
       .subject(this.subject)
       .from(this.from)
       .to(this.recipient)
-      .text(`your token : ${JSON.stringify(this.templateData)}. Layout : ${this.template}`) // TODO use .htmlView(this.template, this.templateData)
+      .htmlView(this.template, this.templateData)
   }
 
   constructor (private actionToken: string, private recipient: string) {}
 
   public async send () {
-    return Mail.sendLater((message) => this.configure(message))
+    return Mail.send((message) => this.configure(message))
   }
 }
