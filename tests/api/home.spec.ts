9e523078f76654136683553420eea621ac5b5629
crbast <bastien.crettenand@hotmail.com>
2021-04-10 20:11:07 +0000

    Fix unit tests

--- tests/api/home.spec.ts
+++ tests/api/home.spec.ts
@@ -3,12 +3,12 @@ import test from 'japa'
 
 test.group('api - status', () => {
   test('Get v1 status', async (assert) => {
-    const result = await axios.get('v1')
+    const result = await axios.get('http://127.0.0.1/v1')
     assert.equal(result.status, 200)
   })
 
   test('404 error testing', async (assert) => {
-    const result = await axios.get('v1324')
+    const result = await axios.get('http://127.0.0.1/v1324')
     assert.equal(result.status, 404)
   })
 })
