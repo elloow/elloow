a1d1f9265b3f51c12b3fb34b478e5916c229836c
crbast <bastien.crettenand@hotmail.com>
2021-04-11 11:30:13 +0000

    Add v1 status test

--- tests/api/home.spec.ts
+++ tests/api/home.spec.ts
@@ -5,8 +5,10 @@ const httpClient = HttpClient.use()
 
 test.group('api - status', () => {
   test('Get v1 status', async (assert) => {
-    const result = await httpClient.get('/v1')
+    const result = await httpClient.get('/')
     assert.equal(result.status, 200)
+    assert.isObject(result.data.APIs)
+    assert.containsAllKeys(result.data.APIs, ['v1'])
   })
 
   test('404 error testing', async (assert) => {
