ec5e1c1fb917f703b4f351a58aa9a9ebb67b8a69
crbast <bastien.crettenand@hotmail.com>
2021-05-09 09:21:52 +0000

    Add dynamic documentation endpoint

--- start/routes/index.ts
+++ start/routes/index.ts
@@ -2,11 +2,18 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 
 import Route from '@ioc:Adonis/Core/Route'
 import Answer from 'App/Helpers/Answer'
+import fs from 'fs'
+import path from 'path'
 
 import './v1'
 
 Route.get('/', async () => {
-  return { APIs: { v1: { route: '/v1', state: 'active' } } }
+  return { APIs: { v1: { route: '/v1', state: 'active', docs: '/docs' } } }
+})
+
+Route.get('/docs', async () => {
+  const data = fs.readFileSync(`${path.dirname(__dirname)}/../insomnia_documentation.json`).toString()
+  return JSON.parse(data)
 })
 
 Route.any('*', async ({ response }: HttpContextContract) => {
