d57de2e4a39433dc54bd14db1071bc07d225c663
Bastien Crettenand <bastien.crettenand@liip.ch>
2022-01-30 11:20:46 +0100

    Fix unit tests (new API response convention)

--- tests/api/v1/home.spec.ts
+++ tests/api/v1/home.spec.ts
@@ -7,7 +7,7 @@ test.group('api/v1 - status', () => {
   test('Get v1 status', async (assert) => {
     const result = await httpClient.get('/v1')
     assert.equal(result.status, 200)
-    assert.isTrue(result.data.success)
-    assert.isTrue(result.data.active)
+    assert.isTrue(result.data['success'])
+    assert.isTrue(result.data['data']['active'])
   })
 })
